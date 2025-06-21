d70df403ba3eb994bdc6c0a4bcd8893936d81f41
biloofranck <biloofranck@example.com>
2025-06-21 23:20:02 +0100

    Project creation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,6 @@ import './global.css';
 
 export default function RootLayout() {
   return <Stack>
-    <Stack.Screen name="products/[id]" options={{ headerShown: false }} />
     <Stack.Screen name="index" options={{ headerShown: false }} />
-    <Stack.Screen name="signup" options={{ headerShown: false }} />
   </Stack>;
 }
